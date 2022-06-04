46dab3cdfd4aafe9695c06af8c76526577cf7a3a
Victor Hogemann <victor.hogemann@gmail.com>
2022-06-04 19:00:04 +0100

    Adding expenses lookup

--- CamaraGQL/CamaraClient.fs
+++ CamaraGQL/CamaraClient.fs
@@ -1,29 +1,56 @@
 module Camara.RestAPI
-    open FSharp.Data
-    type DeputyListResponse = JsonProvider<"json/deputados.json">
-    type DeputyDetailsResponse = JsonProvider<"json/detalhes_deputado.json">
-    let baseUrl = "https://dadosabertos.camara.leg.br/api/v2"
-    
-    // ?id=&nome=&idLegislatura=-37226838&idLegislatura=20933510&siglaUf=velit Ut elit&siglaUf=esse ipsum ullamco voluptate&siglaPartido=velit Ut elit&siglaPartido=esse ipsum ullamco voluptate&siglaSexo=nulla sunt Lorem magna&pagina=41719264&itens=41719264&dataInicio=nulla sunt Lorem magna&dataFim=nulla sunt Lorem magna&ordem=ASC&ordenarPor=nome
-    let DeputyList (siglaUf:string) (siglaPartido:string) = task {
+
+open System
+open FSharp.Data
+
+type DeputyListResponse = JsonProvider<"json/deputados.json">
+type DeputyDetailsResponse = JsonProvider<"json/detalhes_deputado.json">
+type DeputyExpenseResponse = JsonProvider<"json/despesas_deputado.json">
+
+let baseUrl = "https://dadosabertos.camara.leg.br/api/v2"
+
+// ?id=&nome=&idLegislatura=-37226838&idLegislatura=20933510&siglaUf=velit Ut elit&siglaUf=esse ipsum ullamco voluptate&siglaPartido=velit Ut elit&siglaPartido=esse ipsum ullamco voluptate&siglaSexo=nulla sunt Lorem magna&pagina=41719264&itens=41719264&dataInicio=nulla sunt Lorem magna&dataFim=nulla sunt Lorem magna&ordem=ASC&ordenarPor=nome
+let DeputyList (state: string) (party: string) =
+    task {
         let query =
             seq {
-                if siglaUf <> null then yield "siglaUf", siglaUf
-                if siglaPartido <> null then yield "siglaPartido", siglaPartido
+                if state <> null then
+                    yield "siglaUf", state
+
+                if party <> null then
+                    yield "siglaPartido", party
             }
             |> Seq.toList
-        let! response = Http.AsyncRequestString($"{baseUrl}/deputados", query) |> Async.StartAsTask
-        let deputyList =
-            response
-            |> DeputyListResponse.Parse
-            
-        return 
-            deputyList.Dados
+
+        let! response =
+            Http.AsyncRequestString($"{baseUrl}/deputados", query)
+            |> Async.StartAsTask
+
+        let deputyList = response |> DeputyListResponse.Parse
+
+        return deputyList.Dados
     }
-    let rec DeputyDetails (id:int) = task {
-        let! response = Http.AsyncRequestString($"{baseUrl}/deputados/{id}") |> Async.StartAsTask
-        let deputy =
-            response
-            |> DeputyDetailsResponse.Parse
+
+let DeputyDetails (id: int) =
+    task {
+        let! response =
+            Http.AsyncRequestString($"{baseUrl}/deputados/{id}")
+            |> Async.StartAsTask
+
+        let deputy = response |> DeputyDetailsResponse.Parse
+
         return deputy
+    }
+
+let DeputyExpenses (id: int) (year) =
+    task {
+        let query =
+            seq { if year <> null then yield "ano", year }
+            |> Seq.toList
+
+        let! response = Http.AsyncRequestString($"{baseUrl}/deputados/{id}/despesas", query)
+
+        let expenses = response |> DeputyExpenseResponse.Parse
+
+        return expenses.Dados
     }
--- CamaraGQL/Schema.fs
+++ CamaraGQL/Schema.fs
@@ -1,36 +1,62 @@
 module Camara.Schema
 
-    open HotChocolate
-    type Deputy (data:RestAPI.DeputyListResponse.Dado) =
-        member val Id = data.Id with get
-        member val Name = data.Nome with get
-        member val State = data.SiglaUf with get
-        member val Party = data.SiglaPartido with get
-        member _.GetDetails([<Parent>]deputy:Deputy) = task {
+open System
+open System.Dynamic
+open HotChocolate
+
+type Deputy(data: RestAPI.DeputyListResponse.Dado) =
+    member val Id = data.Id
+    member val Name = data.Nome
+    member val State = data.SiglaUf
+    member val Party = data.SiglaPartido
+    member val Picture = data.UrlFoto
+
+    member _.GetDetails([<Parent>] deputy: Deputy) =
+        task {
             let! response = RestAPI.DeputyDetails deputy.Id
             return response.Dados |> DeputyDetails
         }
-    and  DeputyDetails (data:RestAPI.DeputyDetailsResponse.Dados) =
-        member val Id = data.Id with get
-        member val FullName = data.NomeCivil with get
-        member val Cpf = data.Cpf with get
-        member val Education = data.Escolaridade with get
-        member val Gender = data.Sexo with get
-        member val BirthDate = data.DataNascimento
-        member val DeathDate = data.DataFalecimento
-        member val SocialNetworks = data.RedeSocial
         
-    
-    type Query () =
-        member _.Deputies(state:string, party:string) = task {
-            let! response = RestAPI.DeputyList state party
-            return
-                response |> Seq.map Deputy
-        }
+    member _.GetExpenses([<Parent>] deputy: Deputy, year) =
+        task {
+            let! response = RestAPI.DeputyExpenses deputy.Id year
             
-        
-        member _.Deputy(id:int) = task {
-            let! response = RestAPI.DeputyDetails id
             return
-                response.Dados |> DeputyDetails
+                if response <> null then
+                    response
+                    |> Seq.map DeputyExpenses
+                else
+                    Seq.empty
+        }
+
+and DeputyDetails(data: RestAPI.DeputyDetailsResponse.Dados) =
+    member val Id = data.Id
+    member val FullName = data.NomeCivil
+    member val Cpf = data.Cpf
+    member val Education = data.Escolaridade
+    member val Gender = data.Sexo
+    member val BirthDate = data.DataNascimento
+    member val DeathDate = data.DataFalecimento
+    member val SocialNetworks = data.RedeSocial
+
+and DeputyExpenses(data: RestAPI.DeputyExpenseResponse.Dado) =
+    member val Year = data.Ano with get
+    member val SupplierCnpjOrCpf = data.CnpjCpfFornecedor with get
+    member val DocumentCode = data.CodDocumento
+    member val BatchCode = data.CodLote
+    member val DocumentDate = data.DataDocumento
+    member val DocumentNumber = data.NumDocumento
+    member val ReimbursementNumber = data.NumRessarcimento
+    member val InstallmentNumber = data.Parcela
+    member val ExpenseType = data.TipoDespesa
+    member val DocumentType = data.TipoDocumento
+    member val DocumentTypeCode = data.CodTipoDocumento
+    member val DocumentValue = data.ValorDocumento
+    member val OverExpenseValue = data.ValorGlosa
+    member val NetValue = data.ValorLiquido
+type Query() =
+    member _.Deputies(state: string, party: string) =
+        task {
+            let! response = RestAPI.DeputyList state party
+            return response |> Seq.map Deputy
         }
