b8714b1fb970cb21f51322db9395c0c75bad929f
Victor Hogemann <victor@hogemann.com>
2022-11-21 18:54:19 +0000

    Adding mock rest server

---
+++ CamaraGQL.Test/CamaraApiMock.fs
@@ -0,0 +1,10 @@
+﻿namespace CamaraGQL.Test
+
+open RestMockCore
+open RestMockCore.Interfaces
+
+module CamaraApiMock =
+    
+    // https://www.nuget.org/packages/rest-mock-core/
+    let server port =
+        new HttpServer(port)
